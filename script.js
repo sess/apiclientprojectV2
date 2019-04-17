3a8fcb45d1b0d72fe1b9439ab1edc704b1e0dadf
sergio <serj.sanch@gmail.com>
2019-04-17 20:59:48 +0000

    made progress

--- script.js
+++ script.js
@@ -4,20 +4,86 @@
 
 //let url = "https://images-api.nasa.gov/search?q={q}";
 
+let url;
+
+let apod = "https://api.nasa.gov/planetary/apod?api_key=";
+
 let userInput;
 let i;
 
-$(".enter").click(function(){
+$("#inputOne").click(function(){
+
+    $(document).keydown(function(keyPressed) {
+       if (keyPressed.keyCode == 13 && ($("#inputOne").val() != "")) {
+            userInput = $("#inputOne").val();
+            console.log(userInput);
+            
+            $('html,body').animate({
+                scrollTop: $("#pageTwo").offset().top},'slow'
+            );
+            
+            $("#arrowBack").show();
     
-    userInput = $("#inputOne").val();
+            //url = "https://images-api.nasa.gov/search?q=" + userInput;
+        
+            /*$.ajax({
+                url: url,
+                method: "GET",
+                success: function(response){
+                    console.log(url);
+                }
+            });*/
+            
+            $("#inputOne").val("");
+        }
+    });
+});
+
+$("#arrowBack").click(function(){
+    $('html,body').animate({
+        scrollTop: $("#pageOne").offset().top},'slow'
+    );
+            
+    $("#arrowBack").hide();
+});
+
+$("#apodBttn").click(function(){
+    $('html,body').animate({
+        scrollTop: $("#pageTwo").offset().top},'slow'
+    );
+            
+    $("#arrowBack").show();
     
-    url = "https://images-api.nasa.gov/search?q=" + userInput;
-    
-    $.ajax({
+    /*$.ajax({
         url: url,
         method: "GET",
         success: function(response){
             console.log(url);
         }
-    });
+    });*/
+});
+
+$(".enter").click(function(){
+    
+    userInput = $("#inputOne").val();
+    
+    //url = "https://images-api.nasa.gov/search?q=" + userInput;
+    
+    console.log(userInput);
+            
+    $('html,body').animate({
+        scrollTop: $("#pageTwo").offset().top},'slow'
+    );
+            
+    $("#arrowBack").show();
+    
+    /*$.ajax({
+        url: url,
+        method: "GET",
+        success: function(response){
+            console.log(url);
+        }
+    });*/
+    
+    $("#inputOne").val("");
 });
