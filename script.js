8c9c8718b610c1f82a20e9f40615aed9b1d9fd35
sergio <serj.sanch@gmail.com>
2019-04-17 21:10:36 +0000

    updated code

--- script.js
+++ script.js
@@ -1,31 +1,18 @@
 /* global $ */
-
 //let url = "https://api.nasa.gov/planetary/apod?api_key=NeRV1tJp7ld5IHrxZLZbIkky5Eyt5ogvkTRhN4lv";
-
 //let url = "https://images-api.nasa.gov/search?q={q}";
-
+let apodUrl = "https://api.nasa.gov/planetary/apod?api_key=";
 let url;
-
-let apod = "https://api.nasa.gov/planetary/apod?api_key=";
-
 let userInput;
-let i;
 
 $("#inputOne").click(function(){
-
     $(document).keydown(function(keyPressed) {
        if (keyPressed.keyCode == 13 && ($("#inputOne").val() != "")) {
             userInput = $("#inputOne").val();
             console.log(userInput);
-            
-            $('html,body').animate({
-                scrollTop: $("#pageTwo").offset().top},'slow'
-            );
-            
+            $('html,body').animate({scrollTop: $("#pageTwo").offset().top},'slow');
             $("#arrowBack").show();
-    
             //url = "https://images-api.nasa.gov/search?q=" + userInput;
-        
             /*$.ajax({
                 url: url,
                 method: "GET",
@@ -33,17 +20,13 @@ $("#inputOne").click(function(){
                     console.log(url);
                 }
             });*/
-            
             $("#inputOne").val("");
         }
     });
 });
 
 $("#arrowBack").click(function(){
-    $('html,body').animate({
-        scrollTop: $("#pageOne").offset().top},'slow'
-    );
-            
+    $('html,body').animate({scrollTop: $("#pageOne").offset().top},'slow');
     $("#arrowBack").hide();
 });
 
@@ -51,11 +34,9 @@ $("#apodBttn").click(function(){
     $('html,body').animate({
         scrollTop: $("#pageTwo").offset().top},'slow'
     );
-            
     $("#arrowBack").show();
-    
     /*$.ajax({
-        url: url,
+        url: apodUrl,
         method: "GET",
         success: function(response){
             console.log(url);
@@ -64,19 +45,11 @@ $("#apodBttn").click(function(){
 });
 
 $(".enter").click(function(){
-    
     userInput = $("#inputOne").val();
-    
     //url = "https://images-api.nasa.gov/search?q=" + userInput;
-    
     console.log(userInput);
-            
-    $('html,body').animate({
-        scrollTop: $("#pageTwo").offset().top},'slow'
-    );
-            
+    $('html,body').animate({scrollTop: $("#pageTwo").offset().top},'slow');
     $("#arrowBack").show();
-    
     /*$.ajax({
         url: url,
         method: "GET",
@@ -84,6 +57,5 @@ $(".enter").click(function(){
             console.log(url);
         }
     });*/
-    
     $("#inputOne").val("");
 });
