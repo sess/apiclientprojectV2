b77c5235c1752684fa0ccd9fe3d976a57093e707
sergio <serj.sanch@gmail.com>
2019-04-28 23:42:46 +0000

    polished

--- script.js
+++ script.js
@@ -4,13 +4,13 @@
 let apodUrl = "https://api.nasa.gov/planetary/apod?api_key=NeRV1tJp7ld5IHrxZLZbIkky5Eyt5ogvkTRhN4lv";
 let url = "https://images-api.nasa.gov/search?q={q}";
 let userInput;
-let i = 0;
+let i = 1;
 let arrayLength;
 let imgArray= [];
 let descArray= [];
 let titleArray= [];
 
-function progress(){
+/*function progress(){
     $("#back").click(function(){
         i = i - 1;
         
@@ -28,7 +28,7 @@ function progress(){
             i = i;
         }
     });
-}
+}*/
 
 $("#inputOne").click(function(){
     $(document).keydown(function(keyPressed) {
@@ -48,7 +48,6 @@ $("#inputOne").click(function(){
                     $("#text").text(response.collection.items[0].data[0].description);
                 }
             });
-            $("#inputOne").val("");
         }
     });
 });
@@ -57,26 +56,19 @@ $("#arrowBack").click(function(){
     $('html,body').animate({scrollTop: $("#pageOne").offset().top},'slow');
     $("#arrowBack").hide();
     $("#progressArrows").hide();
-    i = 0;
+    //i = 0;
     arrayLength;
     imgArray= [];
     descArray= [];
     titleArray= [];
+    $("#inputOne").val("");
 });
 
-$("#apodBttn").click(function(){
+/*$("#apodBttn").click(function(){
     $('html,body').animate({scrollTop: $("#pageTwo").offset().top},'slow');
     $("#arrowBack").show();
-    $.ajax({
-        url: apodUrl,
-        method: "GET",
-        success: function(response){
-            $("#boldName").text(response.title);
-            $("#imgResult").attr("src",response.url);
-            $("#text").text(response.explanation);
-        }
-    });
-});
+
+});*/
 
 $(".enter").click(function(){
     userInput = $("#inputOne").val();
@@ -93,15 +85,15 @@ $(".enter").click(function(){
             $("#imgResult").attr("src",response.collection.items[0].links[0].href);
             $("#text").text(response.collection.items[0].data[0].description);
             
-            response.collection.items.forEach(function(element){
+            /*response.collection.items.forEach(function(element){
                 imgArray.push(response.collection.items[element].links[0].href);
-            });
+            });*/
             
             /*for(let x = 0; x < response.collection.items[i].length; x++){
                 
             }*/
             
-            for(let x = 0; x < response.collection.items[x].length; x++){
+            /*for(let x = 0; x < response.collection.items[x].length; x++){
                 descArray.push(response.collection.items[x].data[0].description);
             }
             
@@ -112,42 +104,69 @@ $(".enter").click(function(){
             console.log(imgArray);
             console.log(descArray);
             console.log(titleArray);
-            
+            */
         }
     });
-    $("#inputOne").val("");
 });
 
 $("#back").click(function(){
-    console.log("back");
-     $.ajax({
-        url: url,
-        method: "GET",
-        success: function(response){
-            arrayLength = response.collection.items.length;
-            progress();
-            
-            console.log(url);
-            $("#boldName").text(response.collection.items[i].data[0].title);
-            $("#imgResult").attr("src",response.collection.items[i].links[0].href);
-            $("#text").text(response.collection.items[i].data[0].description);
-        }
-    });
+    console.log("previous");
+    userInput = $("#inputOne").val();
+    //url = "https://images-api.nasa.gov/search?q=" + userInput;
+    if(i === 0){
+        alert("There aren't any more entries");
+    }else if(i !== 0){
+         $.ajax({
+            url: url,
+            method: "GET",
+            success: function(response){
+                //arrayLength = response.collection.items.length;
+                //progress();
+                i = i - 1;
+                url = "https://images-api.nasa.gov/search?page=" + i +"&q=" + userInput; 
+                console.log(i);
+                
+                console.log(url);
+                $("#boldName").text(response.collection.items[0].data[0].title);
+                $("#imgResult").attr("src",response.collection.items[0].links[0].href);
+                $("#text").text(response.collection.items[0].data[0].description);
+            }
+        });
+    }
 });
 
 $("#next").click(function(){
     console.log("next");
+    userInput = $("#inputOne").val();
      $.ajax({
         url: url,
         method: "GET",
         success: function(response){
-            arrayLength = response.collection.items.length;
-            progress();
-            
+            i = i + 1;
+            url = "https://images-api.nasa.gov/search?page=" + i +"&q=" + userInput; 
+            console.log(i);
             console.log(url);
-            $("#boldName").text(response.collection.items[i].data[0].title);
-            $("#imgResult").attr("src",response.collection.items[i].links[0].href);
-            $("#text").text(response.collection.items[i].data[0].description);
+            $("#boldName").text(response.collection.items[0].data[0].title);
+            $("#imgResult").attr("src",response.collection.items[0].links[0].href);
+            $("#text").html(response.collection.items[0].data[0].description);
         }
     });
+});
+
+$.ajax({
+        url: apodUrl,
+        method: "GET",
+        success: function(response){
+            if(response.media_type === "video"){
+                $("#imgResultTwo").hide();
+            }
+            
+            if(response.media_type === "image"){
+                $("#imgResultOne").text("<img id=\"imgResult\" src=\"\">");
+            }
+            
+            $("#boldName").text(response.title);
+            $(".imgResult").attr("src",response.url);
+            $("#text").html(response.explanation);
+        }
 });
